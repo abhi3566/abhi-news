256fbf820ee160f4b1aaa38e5e6eda646a088382
Abhishek Kashyap <abhi3566@gmail.com>
2024-05-16 04:11:29 +0530

    Update dbConnect.js
    removed connection string

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://abhishek3566:Abhi3566@devconnector.vxsu7.mongodb.net/abhi-news?retryWrites=true&w=majority'  , {useUnifiedTopology:true , useNewUrlParser:true})
+mongoose.connect('mongodb+srv://abhishek3566, {useUnifiedTopology:true , useNewUrlParser:true})
 
 const connection = mongoose.connection
 
